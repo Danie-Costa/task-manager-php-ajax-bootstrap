d75a60a2973fb1c5857478fb31d8c9cc4ea68f2e
Daniel Pontes <daniel.costa@auditto.online>
2025-04-28 11:47:46 -0300

    modelo rotas web

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,5 @@
 <?php
 
-// $webRoutes = [
-//     '/' => [
-//         'GET' => 'home',
-//     ],
-//     '/sobre' => [
-//         'GET' => 'sobre',
-//     ],
-// ];
-
 class WebRouter implements RouterInterface {
     public function getRoutes(): array {
         return [
